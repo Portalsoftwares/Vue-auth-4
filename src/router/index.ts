6235b6d1219405ab0de11a4d46443fd68136c1b2
rafael sanchez port <rafysanchez@hotmail.com>
2024-03-11 09:58:50 -0300

    Atualizações no roteador de rotas***
    ***Atualizações no roteador de rotas para adicionar redirecionamento e autenticação.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,33 @@
-import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import Register from '@/views/Register.vue'
 
 const routes: Array<RouteRecordRaw> = [
-    {path: '/', component: Home},
-    {path: '/login', component: Login},
-    {path: '/register', component: Register},
-]
+    { path: '/', component: Home },
+    { path: '/login', component: Login },
+    { path: '/register', component: Register },
+    { path: '/:pathMatch(.*)*', redirect: '/' },
+    { path: 'dashboard', component: () => import('../views/Dashboard.vue'), meta: { requiresAuth: true } }
+];
+
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    const loggedIn = localStorage.getItem('user')
+
+    if (to.meta?.requiresAuth) {
+        if (loggedIn) {
+            next('/login')
+            return
+        }
+    }
+    next()
+
 })
 
 export default router
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+<h1> Dashboard</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      message: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
